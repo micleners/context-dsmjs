b5edea175081f105a30e618ca5a6c5ed0b195a32
Michael Leners <micleners@gmail.com>
2021-08-04 20:52:31 -0500

    1 - Prop drilling

---
+++ src/components/AbilityInput.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function AbilityInput({ values, handleChange, name }) {
+  return (
+    <>
+      <h3>{name}</h3>
+      <input
+        type="number"
+        name={name}
+        min="0"
+        required
+        max="100"
+        value={values[name]}
+        onChange={handleChange}
+      />
+    </>
+  );
+}
---
+++ src/components/AttributeInputs.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function AttributeInputs({ values, handleChange }) {
+  return (
+    <div className="input-group">
+      <h3>name</h3>
+      <input
+        type="text"
+        name="name"
+        required
+        value={values.name}
+        onChange={handleChange}
+      />
+
+      <h3>player</h3>
+      <input
+        type="text"
+        name="player"
+        required
+        value={values.player}
+        onChange={handleChange}
+      />
+
+      <h3>background</h3>
+      <textarea
+        type="text"
+        name="background"
+        required
+        value={values.background}
+        onChange={handleChange}
+      />
+
+      <h3>race</h3>
+      <textarea
+        type="text"
+        name="race"
+        required
+        value={values.race}
+        onChange={handleChange}
+      />
+
+      <h3>alignment</h3>
+      <textarea
+        type="text"
+        name="alignment"
+        required
+        value={values.alignment}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
